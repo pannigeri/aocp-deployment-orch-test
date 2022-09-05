241c3e0e61207f324f7817bf3ab77f9cfcdd9511
Yashodhara Shastri G <110095857+yshastrig@users.noreply.github.com>
2022-09-05 15:10:53 +0530

    Update pipelineCaller.groovy

--- vars/pipelineCaller.groovy
+++ vars/pipelineCaller.groovy
@@ -1,16 +1,7 @@
-def call(name, version, feature) {
-    pipeline {
-      agent any
-      stages {
-        stage('Building stage') {
-          steps {
+def call(name, version, feature) {   
             build job: name, 
             parameters: [
             string(name: 'version', value: version),
             extendedChoice(name: 'feature', value: feature)
             ]
-          }
-        }
-      }
-    }
 }
