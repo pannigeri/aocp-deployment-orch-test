d2d3035e406b9b226eee61da5a2f58c69fa38764
Yashodhara Shastri G <110095857+yshastrig@users.noreply.github.com>
2022-09-05 15:17:29 +0530

    Update pipelineCaller.groovy

--- vars/pipelineCaller.groovy
+++ vars/pipelineCaller.groovy
@@ -1,7 +1,11 @@
-def call(name, version, feature) {   
-            build job: name, 
+def call(body) {   
+            def params= [:]
+            body.resolveStrategy = Closure.DELEGATE_FIRST
+            body.delegate = params
+            body()
+            build job: params.name, 
             parameters: [
-            string(name: 'version', value: version),
-            extendedChoice(name: 'feature', value: feature)
+            string(name: 'version', value: params.version),
+            extendedChoice(name: 'feature', value: params.feature)
             ]
 }
