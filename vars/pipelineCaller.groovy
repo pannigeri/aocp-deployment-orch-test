bc3feb31709fc121f0dc815d332272e2081a22ce
Yashodhara Shastri G <110095857+yshastrig@users.noreply.github.com>
2022-09-05 14:41:50 +0530

    vars folder added

---
+++ vars/pipelineCaller.groovy
@@ -0,0 +1,16 @@
+def call(Map pipelineParams) {
+    pipeline {
+      agent any
+      stages {
+        stage('Building stage') {
+          steps {
+            build job: pipelineParams.name, 
+            parameters: [
+            string(name: 'version', value: pipelineParams.version,
+            extendedChoice(name: 'feature', value: pipelineParams.feature)
+            ]
+          }
+        }
+      }
+    }
+}
