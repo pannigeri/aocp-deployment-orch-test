3f3dd0c8a4ae10d61ef8aa868ade05cc2380be85
pannigeri <pannigeri@avaya.com>
2022-09-07 15:35:23 +0530

    Pipex

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,20 +1,18 @@
 def call(String version, String feature, String... pipelines){
     pipelines.each(){
-        pipeline{
-            agent any
-            stages{
-                stage("Building pipeline "){
-                    steps{
-                        pipelineCaller(
-                            name : "${it}",
-                            version : String.valueOf(version),
-                            feature : String.valueOf(feature)
-                        )
-                    }
+        
+        stages{
+            stage("Building pipeline "){
+                steps{
+                    pipelineCaller(
+                        name : "${it}",
+                        version : String.valueOf(version),
+                        feature : String.valueOf(feature)
+                    )
                 }
             }
         }
         
         
-    }
+
 }
