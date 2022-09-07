f48b9373b6ef77da12258d9e1bdb1df0d4a894ef
pannigeri <pannigeri@avaya.com>
2022-09-07 12:56:58 +0530

    Parameters

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,9 +1,9 @@
-def call(String[] pipelines){
+def call(String version, String feature, String... pipelines){
     pipelines.each(){
         pipelineCaller(
             name : "${it}",
-            /* version : String.valueOf(version),
-            feature : String.valueOf(feature) */
+            version : String.valueOf(version),
+            feature : String.valueOf(feature)
         )
         
     }
